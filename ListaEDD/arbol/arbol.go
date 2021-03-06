82966f6e04b6d3c012b50886cb628ed9de75bcfa
Jorge Salazar <201404215@ingenieria.usac.edu.gt>
2021-03-05 22:38:16 -0600

    metodos de arbol realizados

--- ListaEDD/arbol/arbol.go
+++ ListaEDD/arbol/arbol.go
@@ -7,98 +7,176 @@ import (
 // nodo del arbol
 type NodoABB struct {
 	valor int
-	izq *NodoABB
-	der *NodoABB
+	izq   *NodoABB
+	der   *NodoABB
 }
 
 //arbol binario
 type Arbol struct {
 	raiz *NodoABB
+	tam  int
 }
 
 //creando nuevo arbol
-func NewArbol() *Arbol{
-	return &Arbol{nil}
+func NewArbol() *Arbol {
+	return &Arbol{nil, 0}
 }
 
 //Variable estatica que ayuda a verificar si el nodo existe o no
 var Yaesta bool
 
 //metodo para insertar un nuevo nodo
-func (ar *Arbol) InsertarNodoABB(valor int) bool{
-	nuevo := &NodoABB{valor,nil,nil}
+func (ar *Arbol) InsertarNodoABB(valor int) bool {
+	nuevo := &NodoABB{valor, nil, nil}
 	Yaesta = false
-	if ar.raiz == nil{
+	if ar.raiz == nil {
 		fmt.Println("se inserto la raiz")
 		ar.raiz = nuevo
-	}else{
+		ar.tam++
+	} else {
 		// LLamar metodo de insercion recursiva
-		insertarABB(ar.raiz,nuevo)
+		insertarABB(ar.raiz, nuevo)
+
 		return Yaesta
 	}
 	return Yaesta
 }
 
-
-func insertarABB(raiz *NodoABB,nuevo *NodoABB){
-	if nuevo.valor > raiz.valor{
-		if raiz.der == nil{
+func insertarABB(raiz *NodoABB, nuevo *NodoABB) {
+	if nuevo.valor > raiz.valor {
+		if raiz.der == nil {
 			fmt.Println("se inserto a la derecha")
 			raiz.der = nuevo
-		}else{
+		} else {
 			// verificar nueva insecion
-			insertarABB(raiz.der,nuevo)
+			insertarABB(raiz.der, nuevo)
 		}
-	}else if nuevo.valor < raiz.valor{
-		if(raiz.izq == nil){
+	} else if nuevo.valor < raiz.valor {
+		if raiz.izq == nil {
 			fmt.Println("se inserto a la izquierda")
 			raiz.izq = nuevo
-		}else{
+
+		} else {
 			// verificar nueva insercion
-			insertarABB(raiz.izq,nuevo)
+			insertarABB(raiz.izq, nuevo)
 		}
-	}else if nuevo.valor == raiz.valor {
+	} else if nuevo.valor == raiz.valor {
 		fmt.Println("el nodo ya existe")
 		Yaesta = true
 	}
 }
 
 // Metodo para obtener un arbol de manera recursiva
-func  ObtenerNodo(raiz *NodoABB, valor int) *NodoABB{
-
-	if raiz == nil  {
+func ObtenerNodo(raiz *NodoABB, valor int) *NodoABB {
+	if raiz == nil {
 		return nil
-	}else if raiz.valor == valor{
+	} else if raiz.valor == valor {
 		fmt.Println("se encontro el nodo")
-
-		return  raiz
-	}else{
-		if valor > raiz.valor{
-			fmt.Println("se corrio a la derecha")
-			ObtenerNodo(raiz.der,valor)
-		}else if valor < raiz.valor{
-			fmt.Println("se corrido a la izquierda")
-			ObtenerNodo(raiz.izq,valor)
-		}else{
-
-			return  nil
+		return raiz
+	} else {
+		var valor1 *NodoABB
+		if valor > raiz.valor {
+			valor1 = ObtenerNodo(raiz.der, valor)
+		} else if valor < raiz.valor {
+			valor1 = ObtenerNodo(raiz.izq, valor)
 		}
+		return valor1
 	}
-	return nil
 }
 
 // Metodo que obtiene el nodo como tal
 func (ar *Arbol) Obtener(valor int) *NodoABB {
-	retornado := ObtenerNodo(ar.raiz,valor)
-
+	var retornado = ObtenerNodo(ar.raiz, valor)
+	fmt.Println(retornado)
 	return retornado
 }
 
-//Metodo recursivo de profundidad de un arbol
-func profundidad()  {
-	
+//funcion para retornar la cantidad de nodos del arbol
+func (ar *Arbol) CantidadNodos() int {
+	return ar.tam
 }
 
+//Metodo recursivo de profundidad de un arbol
+func profundidad(raiz *NodoABB) int {
+	if raiz == nil {
 
+		return 0
+	} else {
+		var profizq = profundidad(raiz.izq)
+		var profder = profundidad(raiz.der)
 
+		if profizq > profder {
 
+			return profizq + 1
+		} else {
+			return profder + 1
+		}
+	}
+}
+
+// Metodo para retornar el valor de la profundidad
+func (ar *Arbol) RetornarProf() int {
+	profundidad := profundidad(ar.raiz)
+	fmt.Println("La profundidad es: ", profundidad)
+	return profundidad
+}
+
+/*****************************RECORRIDOS**********************/
+//Metodo recursivo para recorrer Inorden
+// IZQ - RAIZ - DERECHA
+func inorden(raia *NodoABB) {
+	if raia.izq != nil {
+		inorden(raia.izq)
+	}
+
+	fmt.Print("nodo: ", raia.valor, "    ")
+
+	if raia.der != nil {
+		inorden(raia.der)
+	}
+}
+
+//metodo que retorna el recorrido
+func (ar *Arbol) RecorridoInorden() {
+	inorden(ar.raiz)
+	fmt.Println("Termino el recorrido")
+}
+
+//Metodo recursivo para recorre Preorder
+func Preorder(raiz *NodoABB) {
+	fmt.Print("nodo: ", raiz.valor, "    ")
+
+	if raiz.izq != nil {
+		Preorder(raiz.izq)
+	}
+
+	if raiz.der != nil {
+		Preorder(raiz.der)
+	}
+}
+
+//metodo que retorna el recorrido
+func (ar *Arbol) RecorridoPreorden() {
+	Preorder(ar.raiz)
+	fmt.Println("Termino el recorrido")
+}
+
+//Metodo recursivo para recorre Postorden
+func postorden(raiz *NodoABB) {
+
+	if raiz.izq != nil {
+		postorden(raiz.izq)
+	}
+
+	if raiz.der != nil {
+		postorden(raiz.der)
+	}
+
+	fmt.Print("nodo: ", raiz.valor, "    ")
+}
+
+//metodo que retorna el recorrido
+func (ar *Arbol) RecorridoPostorden() {
+	postorden(ar.raiz)
+	fmt.Println("Termino el recorrido")
+}
--- ListaEDD/main.go
+++ ListaEDD/main.go
@@ -1,42 +1,24 @@
 package main
 
-import ("./arbol")
+import (
+	"./arbol"
+)
 
 func main() {
 
 	arbol := arbol.NewArbol()
+	arbol.RetornarProf()
 	arbol.InsertarNodoABB(5)
 	arbol.InsertarNodoABB(10)
 	arbol.InsertarNodoABB(1)
 	arbol.InsertarNodoABB(23)
 	arbol.InsertarNodoABB(18)
+	arbol.InsertarNodoABB(18)
+	arbol.InsertarNodoABB(5)
+	arbol.RetornarProf()
 	arbol.Obtener(23)
-
-
-/*	lis := pila.NewPila()
-	listaaa := lista.NewLista()
-
-	lis.Push(2)
-	lis.Push(5)
-	lis.Push(7)
-	lis.Push(1)
-	lis.Push(52)
-	lis.Push(4)
-	lis.Push(73)
-	lis.Mostrar()
-	lis.Graph()
-
-	listaaa.Insertar(12)
-	listaaa.Insertar(14)
-	listaaa.Insertar(15)
-	//listaaa.Imprimir()
-	listaaa.Eliminar(12)
-	//listaaa.Imprimir()
-	listaaa.Eliminar(15)
-	listaaa.Eliminar(14)
-	listaaa.Eliminar(14)
-	//listaaa.Imprimir()*/
-
-
+	arbol.RecorridoInorden()
+	arbol.RecorridoPostorden()
+	arbol.RecorridoPreorden()
 
 }
