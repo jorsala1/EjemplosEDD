4e1099c3f7f039ffed9dc8bfe62c4016bd6f4fca
Josué Carlos Pérez Montenegro <53104989+JosueCPerez@users.noreply.github.com>
2021-09-25 11:53:05 -0600

    Update Matriz.py
    se añadio el metodo elminiar

--- 2S-2021/matriz_python/Matriz.py
+++ 2S-2021/matriz_python/Matriz.py
@@ -59,7 +59,7 @@ class lista_interna:
                 while aux:
                     if nuevo.pos_x < aux.pos_x:
                         nuevo.abajo = aux
-                        nuevo.arriba = aux.ant
+                        nuevo.arriba = aux.arriba
                         aux.arriba.abajo = nuevo
                         aux.arriba = nuevo
                         break
@@ -80,7 +80,57 @@ class lista_interna:
         aux = self.primero
         while aux:
             print("valor =",aux.valor," - x = ",aux.pos_x , " y = ",aux.pos_y)
-            aux = aux.sig              
+            aux = aux.sig         
+
+    def recorrer_y(self):
+        aux = self.primero
+        while aux:
+            print("valor =",aux.valor," - x = ",aux.pos_x , " y = ",aux.pos_y)
+            aux = aux.abajo      
+
+    def eliminar_x(self,pos_y):
+        if self.primero:
+            aux = self.primero
+
+            while(aux):
+                if aux.pos_y == pos_y:
+                    #validamos que posicion estamos eliminando
+                    if(aux == self.primero):
+                        if aux.sig:
+                            aux.sig.ant = None
+                        self.primero = aux.sig
+                        break
+                    else:
+                        aux.ant.sig = aux.sig
+                        if aux.sig:
+                            aux.sig.ant = aux.ant
+                        break
+                else:
+                    aux = aux.sig
+        else:
+            print("no hay datos en la lista")
+
+    def eliminar_y(self,pos_x):
+        if self.primero:
+            aux = self.primero
+            while(aux):
+                if aux.pos_x == pos_x:
+                    #validamos que posicion estamos eliminando
+                    if(aux == self.primero):
+                        if aux.abajo:
+                            aux.abajo.arriba = None
+                        self.primero = aux.abajo
+                        break
+                    else:
+                        aux.arriba.abajo = aux.abajo
+                        if aux.abajo:
+                            aux.abajo.arriba = aux.arriba
+                        break
+                else:
+                    aux = aux.abajo
+        else:
+            print("no hay datos en la lista")
+
 
 #lista cabeceras
 class nodo_cabecera:
@@ -133,7 +183,25 @@ class lista_cabeceras:
         while aux:
             print(aux.posicion)
             aux = aux.sig
-        
+    
+    def eliminar_cabecera(self,dato):
+        if self.primero:
+            aux = self.primero
+            if self.primero.posicion == dato: #eliminar el primero
+                if aux.sig:
+                    aux.sig.ant = None
+                self.primero = aux.sig
+            else:
+                while(aux):
+                    if(aux.posicion == dato):
+                        aux.ant.sig = aux.sig
+                        if(aux.sig):
+                            aux.sig.ant = aux.ant
+                        break
+                    else:
+                        aux = aux.sig
+                    
+                
 
 class matriz:
     def __init__(self, mes):
@@ -185,22 +253,56 @@ class matriz:
                 aux2 = aux2.abajo
                 
             aux = aux.sig
+        
+    def eliminar(self,posx,posy):
+        cabecera_x = self.cabeceras_X.buscar_cabecera(posx)
+        cabecera_y = self.cabeceras_y.buscar_cabecera(posy)
 
-'''
-lista = lista_cabeceras()
-lista.insertar(nodo_cabecera(6))
-lista.insertar(nodo_cabecera(1))
-lista.insertar(nodo_cabecera(2))
+        if(cabecera_x != None and cabecera_y != None):
+            #eliminar en la lista x
+            cabecera_x.lista_interna.eliminar_x(posy)
+            if(cabecera_x.lista_interna.primero == None):
+                self.cabeceras_X.eliminar_cabecera(cabecera_x.posicion)
+                
+            cabecera_y.lista_interna.eliminar_y(posx)
+            if(cabecera_y.lista_interna.primero == None):
+                    self.cabeceras_y.eliminar_cabecera(cabecera_y.posicion)
+        else:
+            print("no existe esa posicion")
 
-lista.recorrer()
 
 
-print("######################")
+
+''' print("######################")
 lista_int = lista_interna()
 lista_int.insertar_x(1,5,6)
 lista_int.insertar_x(2,5,1)
 lista_int.insertar_x(2,5,2)
-lista_int.recorrer() '''
+lista_int.recorrer() 
+
+#prueba listas internas
+lista = lista_interna()
+lista.insertar_y(5,1,1)
+lista.insertar_y(6,2,1)
+lista.insertar_y(7,3,1)
+
+lista.recorrer_y()
+
+lista.eliminar_y(3)
+print("con valores eliminados ****************")
+lista.recorrer_y()
+
+#cabeceras 
+lista = lista_cabeceras()
+lista.insertar(nodo_cabecera(6))
+lista.insertar(nodo_cabecera(1))
+lista.insertar(nodo_cabecera(2))
+lista.recorrer()
+
+print("con valores eliminados ****************")
+lista.eliminar_cabecera(6)
+lista.recorrer()
+'''
 
 #insertar en matriz
 matriz1 = matriz("junio")
@@ -209,5 +311,8 @@ matriz1.insertar(1,5,2)
 matriz1.insertar(2,1,1)
 matriz1.insertar(3,4,1)
 matriz1.insertar(4,4,2)
+matriz1.recorrer_matriz()
 
+print("con valores eliminados ****************")
+matriz1.eliminar(5,2)
 matriz1.recorrer_matriz()
